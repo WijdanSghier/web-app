b735f137370b609b70160c51cf8bacfb2f950e3a
WijdanSghier <wijdanmoner@gmail.com>
2023-03-25 23:58:40 +0200

    new commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: rgb(231, 231, 231);
+        }
+        .main {
+            margin: 0 auto;
+            width: 400px; height: 450px;
+            background-color: rgb(243, 243, 243);
+            box-shadow: #dbdbdb 0px 0px 10px 2px;
+            border-radius: 0.7em;
+            margin-top: 140px;
+        }
+        h3.title {text-align: center; }
+
+        tr > td {
+            background-color: #dbdbdb;
+
+        }
+        .table{
+            margin: 0 auto;
+            direction :rtl;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="main">
+        <br />
+        <h3 class="title">البيانات الشخصية للطالبة وجدان  </h3>
+        <table  class="table">
+            <tr>
+                <td>الاسم</td>
+                <td>وجدان منير الصغير</td>
+            </tr>
+            <tr>
+                <td>تاريخ الميلاد</td>
+                <td>24/1/2000</td>
+            </tr>
+            <tr>
+                <td>البريد الإلكتروني</td>
+                <td>wijdan.moner@gmail.com</td>
+            </tr>
+            <tr>
+                <td>الكلية</td>
+                <td>كلية تقنية المعلومات</td>
+            </tr>
+            <tr>
+                <td>التخصص</td>
+                <td>هندسة برمجيات - SE</td>
+            </tr>
+
+        </table>
+    </div>
+</body>
+</html>
